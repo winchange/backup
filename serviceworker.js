c44c5ef1cfafa3ab8a39164559c5d12e1e054a08
winchange <jorge@winchange.io>
2026-05-25 07:55:27 +0200

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,17 @@
+const version = 851;
+self.addEventListener('install', event => {
+    // eslint-disable-next-line no-console
+    console.log(`Service worker v${version} installed`);
+});
+self.addEventListener('activate', event => {
+    // eslint-disable-next-line no-console
+    console.log(`Service worker v${version} activated`);
+});
+self.addEventListener('fetch', event => {
+    //No cache in service worker
+    if (event.request.method === 'POST' || event.request.method === 'PUT' || event.request.method === 'DELETE') {
+        return;
+    }
+
+    event.respondWith(fetch(event.request));
+});
