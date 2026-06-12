e984bb38c0240f7e4dd360d5ad6e5854dd918a74
winchange <jorge@winchange.io>
2026-06-12 06:42:29 +0200

    Claude Update

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const version = 851;
+const version = 868;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
